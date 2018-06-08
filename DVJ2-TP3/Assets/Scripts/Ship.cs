2648d85a561e77e1a41aec8d74ce6a182fe9fbef
Unknown <jquintanamartin@hotmail.com>
2018-06-08 17:19:51 -0300

    Movimiento Nave

---
+++ DVJ2-TP3/Assets/Scripts/Ship.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    public float speed = 10;
+    public float rotationSpeed = 10;
+    public float angle;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(h * speed, 0, v * speed);
+    
+        Vector3 lastPosition = transform.position;
+
+        Vector3 newPosition = transform.position + movement * Time.deltaTime;
+
+        float newAngleY = GetRealAngle(lastPosition, newPosition);
+
+        Quaternion currentRotation = transform.rotation;
+        Quaternion newRotation = Quaternion.Slerp(currentRotation, Quaternion.AngleAxis(newAngleY, Vector3.up),
+            rotationSpeed * Time.deltaTime);
+
+        transform.position = newPosition;
+        if (h != 0 || v != 0)
+        {
+            angle = newAngleY;
+            transform.rotation = newRotation;
+        }
+    }
+
+    private float GetRealAngle(Vector3 from, Vector3 to)
+    {
+        Vector3 right = Vector3.right;
+
+        Vector3 dir = to - from;
+
+        float angleBtw = Vector3.Angle(right, dir);
+        Vector3 cross = Vector3.Cross(right, dir);
+        if (cross.y < 0)
+        {
+            angleBtw = 360 - angleBtw;
+        }
+
+        return angleBtw;
+    }
+}
